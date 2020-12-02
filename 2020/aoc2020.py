41044d005ef3c4ed1025a0577878dab5fca3851a
Loïc Gremaud <loic.gremaud@arcanite.ch>
2020-12-02 11:16:45 +0100

    Day 1 Part 2, day 2 part 1 and 2 of 2020

--- 2020/aoc2020.py
+++ 2020/aoc2020.py
@@ -1,6 +1,10 @@
 from GenericClass.AdventOfCode import AdventOfCodeYear
 
 
+import re
+from itertools import product
+
+
 class Year2020(AdventOfCodeYear):
 
     def day1(inputs):
@@ -11,5 +15,34 @@ class Year2020(AdventOfCodeYear):
                 if l + s == 2020:
                     return s * l
 
+    def day1_2(inputs):
+        ls = [int(i) for i in inputs]
+
+        for a, b, c in product(ls, ls, ls):
+            if a + b + c == 2020:
+                return a * b * c
+
     def day2(inputs):
-        return ""
+        t = 0
+
+        for i in inputs:
+            _min, _max, letter, password = re.search('(\d+)-(\d+) (\w): (\w+)', i).groups()
+
+            if int(_min) <= password.count(letter) <= int(_max):
+                t += 1
+
+        return t
+
+    def day2_2(inputs):
+        t = 0
+
+        for i in inputs:
+            p1, p2, letter, password = re.search('(\d+)-(\d+) (\w): (\w+)', i).groups()
+            p1, p2 = int(p1), int(p2)
+
+            if p1 - 1 <= len(password) and p2 - 1 <= len(password):
+
+                if (password[p1 - 1] == letter) ^ (password[p2 - 1] == letter):
+                    t += 1
+
+        return t
