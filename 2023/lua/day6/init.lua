97c086c07b09a17bfa3584d015dab49ce75954ca
Loïc Gremaud <loic.gremaud@arcanite.ch>
2023-12-06 06:11:37 +0100

    Day 6: quick and dirty

---
+++ 2023/lua/day6/init.lua
@@ -0,0 +1,38 @@
+local M = {}
+
+local lib = require 'lib'
+local daylib = require 'lib.day6'
+local inspect = require 'inspect'
+
+function M.day(input, debug)
+    local part_1 = 0
+    local part_2 = 0
+
+    local times = {}
+    local distances = {}
+
+    for i in input:split("\n")[1]:gmatch("(%d+)") do
+        table.insert(times, tonumber(i))
+    end
+
+    for i in input:split("\n")[2]:gmatch("(%d+)") do
+        table.insert(distances, tonumber(i))
+    end
+
+    part_1 = daylib.run(times, distances)
+
+    local xtime = ''
+    local xdistance = ''
+    for _, t in pairs(times) do
+        xtime = xtime .. tostring(t)
+    end
+
+    for _, d in pairs(distances) do
+        xdistance = xdistance .. tostring(d)
+    end
+    part_2 = daylib.run({tonumber(xtime)}, {tonumber(xdistance)})
+
+    return {part_1, part_2}
+end
+
+return M
---
+++ 2023/lua/lib/day6.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+function M.run(times, distances)
+    local tt = 1
+    for i, time in pairs(times) do
+        local w = 0
+        for j = 0, time - 1 do
+            if j * (time - j) > distances[i] then
+                w = w + 1
+            end
+        end
+        tt = tt * w
+    end
+    return tt
+end
+
+return M
